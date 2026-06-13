c5e861bb7d60cdb9fe4e0011a959b8e46982e9c2
OmUmale19 <omumale127@gmail.com>
2026-06-13 11:29:41 +0530

    Fix database setup issue: Remove old Postgres migrations, use SQLite

--- prisma/migrations/20260613000000_add_user_profile_fields/migration.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "avatar_url" TEXT;
-ALTER TABLE "users" ADD COLUMN "job_title" TEXT;
-ALTER TABLE "users" ADD COLUMN "company" TEXT;
-ALTER TABLE "users" ADD COLUMN "location" TEXT;
-ALTER TABLE "users" ADD COLUMN "bio" TEXT;
-ALTER TABLE "users" ADD COLUMN "email_notifications" BOOLEAN NOT NULL DEFAULT true;
-ALTER TABLE "users" ADD COLUMN "weekly_digest" BOOLEAN NOT NULL DEFAULT true;
-ALTER TABLE "users" ADD COLUMN "product_updates" BOOLEAN NOT NULL DEFAULT false;
